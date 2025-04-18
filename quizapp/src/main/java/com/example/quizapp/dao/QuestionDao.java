e609ee0134f522bb0031ff6484394649c008d239
Sarthak-Jangam <sarthakjangam73@gmail.com>
2025-04-18 11:58:13 +0530

    Update QuestionDao.java

--- quizapp/src/main/java/com/example/quizapp/dao/QuestionDao.java
+++ quizapp/src/main/java/com/example/quizapp/dao/QuestionDao.java
@@ -12,6 +12,4 @@ public interface QuestionDao extends JpaRepository<Question,Integer> {
 
     List<Question> findByCategory(String category);
 
-    @Query(value = "SELECT * FROM questions q WHERE q.category = :category ORDER BY RANDOM() LIMIT :numQ", nativeQuery = true)
-    List<Question> findRandomQuestionsByCategory(String category,int numQ);
 }
